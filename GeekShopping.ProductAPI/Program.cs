5cdb8e330fa5bb4d277a57167eddd84747b48808
Irineu Junior Pinheiro dos Santos <irineus@gmail.com>
2022-05-13 14:25:27 -0300

    Base and product entities

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,32 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Connect to Azure Key Vault
+var keyVaultEndpoint = builder.Configuration["AzureKeyVault:Endpoint"];
+var keyVaultTenantId = builder.Configuration["AzureKeyVault:TenantId"];
+var keyVaultClientId = builder.Configuration["AzureKeyVault:ClientId"];
+var keyVaultClientSecret = builder.Configuration["AzureKeyVault:ClientSecret"];
+
+var credential = new ClientSecretCredential(keyVaultTenantId, keyVaultClientId, keyVaultClientSecret);
+
+var client = new SecretClient(new Uri(keyVaultEndpoint), credential);
+
+builder.Configuration.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
+
+// Connect to Azure MySQL Database
+var connectionString = builder.Configuration.GetConnectionString("GeekShoppingContext");
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(connectionString,
+        ServerVersion.AutoDetect(connectionString)));
+        /*,
+        builder => builder.MigrationsAssembly("GeekShopping")));*/
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 
